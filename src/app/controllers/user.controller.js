76ca8d3d7729a09248a07717737deef6b99f0503
nonybright <nonybrighto@gmail.com>
2019-05-09 12:37:14 +0100

    change password reset route

--- src/app/controllers/user.controller.js
+++ src/app/controllers/user.controller.js
@@ -14,7 +14,33 @@ async function list(req, res, next){
     
 }
 
-export default {list};
+async function changePassword(req, res, next){
+
+    try{
+        let oldPassword = req.body.oldPassword;
+        let newPassword = req.body.newPassword;
+
+        // @ts-ignore
+        let user = await User.canLogin(req.user.username, oldPassword);
+        if(user){
+                // @ts-ignore
+               let changed = await user.changePassword(newPassword);
+               if(changed){
+                    res.sendStatus(200);
+               }else{
+                    throw new Error('Could not change password');
+               }
+        }else{
+            next(createError(httpStatus.FORBIDDEN, 'not permitted to change password'));
+        }
+    }catch(error){
+        console.log(error);
+        next(createError('Error occured while changing password'));
+    }
+
+}
+
+export default {list, changePassword};
 
 
 
